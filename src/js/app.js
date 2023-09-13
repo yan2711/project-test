9d2963a429f53ae08ee4054a40612ba7c50a4d3a
yan2711 <yan271105@gmail.com>
2023-09-13 21:40:28 +0300

    first commit

---
+++ src/js/app.js
@@ -0,0 +1,5 @@
+let btn = document.querySelectorAll('.block-last__button');
+let modal = document.querySelector('.modal')
+btn.addEventListener('click', () => {
+    modal.style.dysplay = block;
+})
